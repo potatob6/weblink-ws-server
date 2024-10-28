c37fa2919088c739de1eb6cf11acc86fc3d4a558
Zach <myzzzyearz@outlook.com>
2024-10-28 16:43:00 +0800

    add HOSTNAME var

--- src/var.ts
+++ src/var.ts
@@ -1,4 +1,5 @@
 export const LOG_LEVEL = process.env["LOG_LEVEL"] || "info";
+export const HOSTNAME = process.env["HOSTNAME"] || "0.0.0.0";
 export const PORT = parseInt(process.env["PORT"] || "9000", 10);
 export const HEARTBEAT_INTERVAL = parseInt(process.env["HEARTBEAT_INTERVAL"] || "30000", 10);
 export const PONG_TIMEOUT = parseInt(process.env["PONG_TIMEOUT"] || "60000", 10);
